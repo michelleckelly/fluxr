f508c147b684eb8de2751864b24a4376e3974381
Michelle Catherine Kelly <32774848+michelleckelly@users.noreply.github.com>
2025-09-10 17:16:43 -0600

    Comment out test file for now

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-library(testthat)
-library(fluxr)
+#library(testthat)
+#library(fluxr)
 
-test_check("fluxr")
+#test_check("fluxr")
