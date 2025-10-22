0712e20670a6a9b813bb88a822474ca8f36aec85
Michelle Catherine Kelly <32774848+michelleckelly@users.noreply.github.com>
2025-10-22 15:15:20 -0600

    Add default settings for N2 flux

--- R/set_specs.R
+++ R/set_specs.R
@@ -3,7 +3,11 @@
 #' This function is based on the function of the same name from 
 #' `streamMetabolizer`, just simplified to only parameters relavant for 
 #' our model type (Bayes, observation error, reaeration depends on modeled gas
-#' at downstream station). Default values are the same as `streamMetabolizer`.
+#' at downstream station).  
+#' 
+#' Default values for GPP, ER, and K600 are the same as `streamMetabolizer`. 
+#' Default values for N2consume and DN are the same as those in Nifong et al. 
+#' 2020.
 #' 
 set_specs <- function(
     model_name,
@@ -11,22 +15,27 @@ set_specs <- function(
     keep_mcmc = TRUE,
     keep_mcmc_data = TRUE,
     
-    # Hyperparameters for non-hierarchial GPP, ER
+    # Default hyperparameters for non-hierarchial GPP, ER
     GPP_daily_mu = 3.1,
     GPP_daily_lower = -Inf,
     GPP_daily_sigma = 6.0,
-    #alpha_meanlog = -4.6
-    #alpha_sdlog = 0.5,
-    #Pmax_mu = 10,
-    #Pmax_sigma = 7,
     ER_daily_mu = -7.1,
     ER_daily_upper = Inf,
     ER_daily_sigma = 7.1,
     
+    # Default hyperparameters for N2consume, DN
+    # As used in Nifong et al 2020
+    N2consume_daily_mu = -1,
+    N2consume_daily_lower = -Inf,
+    N2consume_daily_sigma = 5,
+    DN_daily_mu = 1,
+    DN_daily_upper = Inf,
+    DN_daily_sigma = 5,
+    
     # We're not pooling K600 (we only have 1 day of data)
     # Therefore hyperparameters for non-hierarchial K600
     K600_daily_meanlog = log(12),
-    K600_daily_sdlog = 0.05, # not totally sure this is correct
+    K600_daily_sdlog = 0.05,
     
     # hyperparameters for error terms
     err_obs_iid_sigma_scale = 0.03,
@@ -39,33 +48,21 @@ set_specs <- function(
     thin_steps = 1,
     verbose = FALSE){
   
-  # Names of input parameters for model
+  # Set common input parameters for all models
   params_in <- 
-    c("GPP_daily_mu", "GPP_daily_lower", "GPP_daily_sigma",
-      "ER_daily_mu", "ER_daily_upper", "ER_daily_sigma",
-      "K600_daily_meanlog", "K600_daily_sdlog", "err_obs_iid_sigma_scale")
+    c("K600_daily_meanlog", "K600_daily_sdlog", "err_obs_iid_sigma_scale")
   
-  # Names of output parameters for model
-  params_out <- 
-    c("GPP", "ER", "DO_R2", "GPP_daily", "ER_daily", "K600_daily",
-      "err_obs_iid_sigma", "err_obs_iid_sigma_scaled",
-      "DO_mod_down", "GPP_inst", "ER_inst", "KO2_inst", "err_obs_iid")
+  # Set common output parameters for all models
+  params_out <-
+    c("K600_daily", "err_obs_iid_sigma", "err_obs_iid_sigma_scaled",
+      "err_obs_iid")
   
+  # Set common contents for specification list
   specsList <- list(
     model_name = model_name,
     
-    params_in = params_in,
-    params_out = params_out,
-    
     keep_mcmc = keep_mcmc,
     keep_mcmc_data = keep_mcmc_data,
-    
-    GPP_daily_mu = GPP_daily_mu,
-    GPP_daily_lower = GPP_daily_lower,
-    GPP_daily_sigma = GPP_daily_sigma,
-    ER_daily_mu = ER_daily_mu,
-    ER_daily_upper = ER_daily_upper,
-    ER_daily_sigma = ER_daily_sigma,
     
     K600_daily_meanlog = K600_daily_meanlog,
     K600_daily_sdlog = K600_daily_sdlog, 
@@ -79,6 +76,56 @@ set_specs <- function(
     thin_steps = thin_steps,
     verbose = verbose
   )
+  
+  # Set additional parameters for O2 models & additional outputs for specsList
+  if(model_name == "o2_twostation"){
+    
+    params_in <- 
+      c(params_in, 
+        "GPP_daily_mu", "GPP_daily_lower", "GPP_daily_sigma",
+        "ER_daily_mu", "ER_daily_upper", "ER_daily_sigma")
+    
+    params_out <-
+      c(params_out,
+        "GPP", "ER", "DO_R2", "GPP_daily", "ER_daily", 
+        "KO2_inst", "DO_mod_down", "GPP_inst", "ER_inst")
+    
+    # Add model-specific parameters to specification list
+    specsList$params_in <- params_in
+    specsList$params_out <- params_out
+    
+    specsList$GPP_daily_mu <- GPP_daily_mu
+    specsList$GPP_daily_lower <- GPP_daily_lower
+    specsList$GPP_daily_sigma <- GPP_daily_sigma
+    specsList$ER_daily_mu <- ER_daily_mu
+    specsList$ER_daily_upper <- ER_daily_upper
+    specsList$ER_daily_sigma <- ER_daily_sigma
+  }
+  
+  # Set additional parameters for N2 models, output specsList
+  if(model_name == "n2_twostation_nifong"){
+    
+    params_in <- 
+      c(params_in, 
+        "N2consume_daily_mu", "N2consume_daily_lower", "N2consume_daily_sigma",
+        "DN_daily_mu", "DN_daily_upper", "DN_daily_sigma")
+    
+    params_out <-
+      c(params_out,
+        "N2consume", "DN", "N2_R2", "N2consume_daily", "DN_daily", 
+        "KN2_inst", "N2_mod_down", "N2consume_inst", "DN_inst")
+    
+    specsList$params_in <- params_in
+    specsList$params_out <- params_out
+    
+    specsList$N2consume_daily_mu <- N2consume_daily_mu
+    specsList$N2consume_daily_lower <- N2consume_daily_lower
+    specsList$N2consume_daily_sigma <- N2consume_daily_sigma
+    specsList$DN_daily_mu <- DN_daily_mu
+    specsList$DN_daily_upper <- DN_daily_upper
+    specsList$DN_daily_sigma <- DN_daily_sigma
+  }
+  
   return(specsList)
 }
 
